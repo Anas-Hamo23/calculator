2381df667d4231d74644c31c1072155ffc924313
Anas Hamo <anas.hamo.023@gmail.com>
2022-09-22 22:52:45 +0300

    Add file

---
+++ main.js
@@ -0,0 +1,7 @@
+const a0 = document.getElementById('a1'),
+    a2 = document.getElementById('a2'),
+    a3 = document.getElementById('a3'),
+    a4 = document.getElementById('a4'),
+    a = document.querySelectorAll('.box'),
+    input = document.getElementById('input').value;
+console.log(input);
