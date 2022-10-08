902b5356802e30732e05c0c92500d9f624f89c0d
Anas Hamo <anas.hamo.023@gmail.com>
2022-10-08 20:01:09 +0300

    Update

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-const a0 = document.getElementById('a1'),
-    a2 = document.getElementById('a2'),
-    a3 = document.getElementById('a3'),
-    a4 = document.getElementById('a4'),
-    a = document.querySelectorAll('.box'),
-    input = document.getElementById('input');
+const output = document.getElementById('calc');
+const display = (props) => {
+    output.value += props;
+}
+const cl = () => {
+    output.value = '';
+}
+const del = () => {
+    output.value = output.value.slice(0, -1);
+}
+const calc = () => {
+    try {
+        output.value = eval(output.value);
+    } catch (err) {
+        output.value = 'Error';
+    }
+}
